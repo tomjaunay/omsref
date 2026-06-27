bdcb0dd3d2cc9e15881dc54ae40d87990b02fc1b
tomjaunay <tomjaunay@oromaxnsw.com.au>
2026-06-27 15:20:24 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
 import Dashboard from '@/components/Dashboard'
 
 export default function Home() {
-  const cookieStore = cookies()
-  const auth = cookieStore.get('auth')
-  const password = process.env.SITE_PASSWORD ?? 'changeme'
-  
-  if (auth?.value !== password) {
-    redirect('/login')
-  }
-
   return <Dashboard />
 }
